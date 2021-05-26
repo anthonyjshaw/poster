5478c04d0bf638e4e3a51f0554342faaa7d797ee
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-26 19:48:19 +0100

    Added index and posts routes

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) =>{
-  res.send('Hello!')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 
+});
+
+app.get('/posts', (req, res) => {
+  res.sendFile(path.join(__dirname, 'posts.html'));
 });
 
 app.listen(3000);

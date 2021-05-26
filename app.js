a28f14aab6b02b22d8d3b9985ca4c1d594aa16e1
Anthony Shaw <anthonyshaw@sceaga.com>
2021-05-26 19:50:48 +0100

    Added pages folder

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'pages/index.html'));
 
 });
 
 app.get('/posts', (req, res) => {
-  res.sendFile(path.join(__dirname, 'posts.html'));
+  res.sendFile(path.join(__dirname, 'pages/posts.html'));
 });
 
 app.listen(3000);
